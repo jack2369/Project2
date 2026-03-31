e9d5717a91b9e0540210ed602c12b43ad8760f93
jack2369 <56374303+jack2369@users.noreply.github.com>
2026-03-31 18:41:52 -0400

    first commit

---
+++ dynamicarray.cc
@@ -0,0 +1,87 @@
+#include "dynamicarray.h"
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::ostream;
+using std::endl;
+// regular constuctor
+DynamicArray::DynamicArray(int size = 1) {
+    values_ = new int[size];
+    for (int i = 0; i < size; i++) {
+        values_[i] = 0;
+    }
+}
+
+// copy constructor
+DynamicArray::DynamicArray(const DynamicArray& d) {
+    values_ = new int[size_];
+    for(int i = 0; i < size_; ++i) {
+        values_[i] = d.values_[i];
+    }
+}
+
+bool DynamicArray::operator ==(DynamicArray& d) const {
+    // if the sizes don't match
+    if(d.size_ != size_) {
+        return false;
+    }
+    // checks each value
+    for(int i = 0; i < size_; ++i) {
+        if(values_[i] != d.values_[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+DynamicArray& DynamicArray::operator=(const DynamicArray &d) {
+    if (this != &d) {
+        int *temp = new int[d.size_];
+        for (int i = 0; i < d.size_; i++) {
+            temp[i] = d.values_[i];
+        }
+
+        delete[] values_;
+        values_ = temp;
+        size_ = d.size_;
+    }
+    return *this;
+}
+
+
+int DynamicArray::RemoveAll(int target) {
+    // counts every instance of the target int
+    int count = 0;
+    for(int i = 0; i < size_; ++i) {
+        if(values_[i] == target) {
+            ++count;
+        }
+    }
+    // makes temporary pointer to hold new array
+    int *temp = new int[size_-count];
+    for(int i = 0, j = 0; i < (size_); ++i) {
+        if(values_[i] != target) {
+            temp[j] = values_[i];
+            ++j;
+        }
+    }
+    // swaps temp with the actual array and then updates size
+    delete [] values_;
+    values_ = temp;
+    size_ -= count;
+    return count;
+}
+
+void DynamicArray::RemoveDuplicates() {
+    for(int i = 0; i < size; ++i) {
+        
+    }
+}
+
+void DynamicArray::SetDelimiter(char delimeter) {
+    delimiter_ = delimeter;
+}
+
+std::ostream &operator<<(std::ostream &ostream, const DynamicArray &d) {
+     
+}
---
+++ dynamicarray.h
@@ -0,0 +1,60 @@
+#include <iostream>
+
+class DynamicArray {
+ public:
+
+  explicit DynamicArray(int size = 1);
+
+  DynamicArray(const DynamicArray& d);
+
+  bool operator ==(DynamicArray& d) const;
+
+  // version that allows one to use the operator in a non-constant setting
+  // to update the values in the array
+  int& operator[](int i) {
+    if ( i >= 0 && i < size_ ) return values_[i];
+    if ( i < 0 ) return values_[0];
+    return values_[size_ - 1];
+  }
+
+  // version that allows one to use the operator on a constant array
+  // returning copies of a value in the array
+  int operator[](int i) const {
+    if ( i >= 0 && i < size_ ) return values_[i];
+    if ( i < 0 ) return values_[0];
+    return values_[size_ - 1];
+  }
+
+  DynamicArray &operator=(const DynamicArray &d);
+
+  void SetSize(int nSize, bool copy = true);
+
+  int GetSize() const {return size_;}
+
+  int RemoveAll(int target);
+
+  void RemoveDuplicates();
+  
+  bool AllUnique() const;
+
+  void FindAndReplace(int find, int replace);
+
+  void Sort(bool descending = false);
+    
+  static void SetDelimiter(char delimeter);
+
+  static char GetDelimiter() {return delimiter_;}
+
+  int Operations(char op = '+');
+
+  ~DynamicArray() {delete [] values_;} //destructor to avoid memleaks
+  
+ private:
+  int size_;
+  int * values_;
+  static char delimiter_; // (for separator used by <<)
+  
+  friend std::ostream &operator<<(std::ostream &ostream, const DynamicArray &d);
+};
+
+std::ostream &operator<<(std::ostream &ostream, const DynamicArray &d);
